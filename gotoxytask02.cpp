76e8b6e62d049f30da6369928d3e657b57ffb8dd
Noor-e-Hira979 <144408192+Noor-e-Hira979@users.noreply.github.com>
2023-10-01 20:22:37 +0500

    Add files via upload

---
+++ gotoxytask02.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+using namespace std;
+void printnameh();
+void printnamea();
+void printnames();
+void printnamen();
+
+int main()
+{
+
+printnameh();
+
+printnamea();
+
+printnames();  
+
+printnames();
+
+printnamea();
+
+printnamen();
+}
+void printnameh()
+{
+cout<<"H "<<endl;
+
+}
+void printnamea()
+{
+cout<<"A "<<endl;
+
+}
+void printnames()
+{
+cout<<"S "<<endl;
+
+}
+void printnamen()
+{
+cout<<"N "<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
